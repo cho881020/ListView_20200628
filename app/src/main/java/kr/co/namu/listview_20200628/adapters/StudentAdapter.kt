28973db8d17067d40f53f6a038630b041a273828
조경진 <kj_cho@idealidea.co.kr>
2020-06-28 19:39:07 +0900

    학생 목록 실제로 추가

--- app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
@@ -15,25 +15,12 @@ class StudentAdapter(val mContext:Context, val resId:Int, val mList:ArrayList<St
 //    한줄씩 직접 그려주는 함수
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-//        convertView : 기존에 그려둔 줄이 있나 (재활용 할게 있나) 판단하는 재료.
-
         var tempRow = convertView
 
-//        null : 없다 를 의미하는 키워드.
-//        재활용할 뷰가 없나요? 질문.
-
         if (tempRow == null) {
-
-//            재활용 할게 없다면, XML을 새로 그려서 tempRow에 대입.
-
             tempRow = inf.inflate(R.layout.student_list_item, null)
         }
-
-//        재활용이건, 새로그렸건 tempRow 내부에는 무조건 뭔가 들어있게 됨. (비어있지 않다)
-//        row에는 절대 null일 일이 없다고 하면서 대입.
-
         val row = tempRow!!
-
 
         return row
     }
