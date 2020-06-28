e03b849338b1389716704b8dbcd61702d71047e7
조경진 <kj_cho@idealidea.co.kr>
2020-06-28 20:02:40 +0900

    학생 이름 / 생년 실제로 출력

--- app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/namu/listview_20200628/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.namu.listview_20200628.R
 import kr.co.namu.listview_20200628.datas.Student
 
@@ -13,6 +14,7 @@ class StudentAdapter(val mContext:Context, val resId:Int, val mList:ArrayList<St
     val inf = LayoutInflater.from(mContext)
 
 //    한줄씩 직접 그려주는 함수
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         var tempRow = convertView
@@ -21,6 +23,20 @@ class StudentAdapter(val mContext:Context, val resId:Int, val mList:ArrayList<St
             tempRow = inf.inflate(R.layout.student_list_item, null)
         }
         val row = tempRow!!
+
+//        row -> student_list_item XML에서 어댑터kt파일로 뷰를 가져오자.
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        상황에 맞는 (=> postion에 들어있는 값에 맞는) 데이터를 출력
+
+//        목록중 위치에 맞는 데이터를 뽑아서 변수에 저장
+        val studentData = mList[position]
+
+//        뽑은 데이터를 텍스트뷰에 반영
+        nameTxt.text = studentData.name
+
+        ageTxt.text = studentData.birthYear.toString()
 
         return row
     }
